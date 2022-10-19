89e2f2d702740b2abd4b1bcf0024c84ed95c3d71
yuanzhihai <396751927@qq.com>
2022-10-19 15:45:44 +0800

    init

--- src/facade/Aliyun.php
+++ src/facade/Aliyun.php
@@ -4,6 +4,15 @@ namespace yzh52521\Aliyun\facade;
 
 use think\Facade;
 
+/**
+ * Class Aliyun
+ * @package think\facade
+ * @mixin \yzh52521\Aliyun\Aliyun
+ * @method static service(string $name = null) ,null|string
+ * @method static mixed getConfig(null|string $name = null, mixed $default = null) 获取配置
+ * @method static array getServiceConfig(string $disk, null $name = null, null $default = null) 获取服务配置
+ * @method static string|null getDefaultDriver() 默认驱动
+ */
 class Aliyun extends Facade
 {
     /**
